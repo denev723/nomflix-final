e3215260c8f9534403d7eea5f00acb4ba76a9a47
denev <65989857+deneb0723@users.noreply.github.com>
2021-03-21 01:12:42 +0900

    route 생성

---
+++ src/Components/Router.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+import Home from "Routes/Home";
+import TV from "Routes/TV";
+import Search from "Routes/Search";
+
+export default () => (
+  <Router>
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/tv" exact component={TV} />
+      <Route path="/search" exact component={Search} />
+      <Redirect from="*" to="/" />
+    </Switch>
+  </Router>
+);
