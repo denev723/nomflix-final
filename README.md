b1f772b2b5554c97ba5af7185cc2031b2f99e0a6
denev <65989857+deneb0723@users.noreply.github.com>
2021-03-23 12:10:10 +0900

    api networking 설정

--- README.md
+++ README.md
@@ -1,9 +1,25 @@
 # nomflix
 영화 소개 앱 (with React) - nomadcoders
+
 ## 화면 구성
-- 영화 리스트
-- TV 드라마 리스트
-- 검색
-- 상세보기
+- [ ] 영화 리스트
+- [ ] TV 드라마 리스트
+- [ ] 검색
+- [ ] 상세보기
+
+## API 정보
+- TV
+- [ ] Top Rated
+- [ ] Popular
+- [ ] Airing Today
+- [ ] Detail
+- [ ] Search
+- Movie
+- [ ] Popular
+- [ ] Now Playing
+- [ ] Upcoming
+- [ ] Detail
+- [ ] Search
+
 
 
---
+++ src/api.js
@@ -0,0 +1,41 @@
+import axios from "axios";
+
+const api = axios.create({
+  baseURL: "https://api.themoviedb.org/3/",
+  params: {
+    api_key: "24e57914be24aef87dfd80b279f9bb90",
+    language: "en-US",
+  },
+});
+
+export const tvApi = {
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_today"),
+  dramaDetail: (id) =>
+    api.get(`tv/${id}`, {
+      params: {
+        append_to_response: "videos",
+      },
+    }),
+  serch: (term) =>
+    api.get("search/tv", {
+      query: encodeURIComponent(term),
+    }),
+};
+
+export const movieApi = {
+  popular: () => api.get("movie/popular"),
+  nowPlaying: () => api.get("movie/now_playing"),
+  upcoming: () => api.get("movie/upcoming"),
+  movieDetail: () =>
+    api.get(`movie/${id}`, {
+      params: {
+        append_to_response: "videos",
+      },
+    }),
+  search: (term) =>
+    api.get("search/movie", {
+      query: encodeURIComponent(term),
+    }),
+};
